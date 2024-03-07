31b64880dd3b1056c20e5bfcc8f3041222ac5953
XxGladiatorxX1 <117952565+XxGladiatorxX1@users.noreply.github.com>
2024-03-06 22:23:41 -0500

    Revised AutoAlign command

--- src/main/java/frc/team2641/robot2024/commands/auto/AutoAlign.java
+++ src/main/java/frc/team2641/robot2024/commands/auto/AutoAlign.java
@@ -2,71 +2,83 @@
 package frc.team2641.robot2024.commands.auto;
 
 import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.team2641.robot2024.subsystems.Drivetrain;
 
 public class AutoAlign extends Command {
 
-    public int element;
     private Drivetrain drivetrain;
-    public static double angularVelocity;
     private int targetAngle;
+    private int oppositeAngle;
     BooleanPublisher alignmentPub;
+    DoublePublisher angularVelocityPub;
 
     public AutoAlign(int element) {
         drivetrain = Drivetrain.getInstance();
-        this.element = element;
 
         NetworkTable table = NetworkTableInstance.getDefault().getTable("state");
 
         alignmentPub = table.getBooleanTopic("autoAlign").publish();
         alignmentPub.set(false);
+
+        angularVelocityPub = table.getDoubleTopic("angularVelocity").publish();
+        angularVelocityPub.set(0);
+
+        if (element == 1) {
+            targetAngle = -90;
+            oppositeAngle = 90;
+            // amp 
+        }
+        else if (element == 2) {
+            targetAngle = 0;
+            oppositeAngle = 180;
+            // speaker 
+        }
+        else if (element == 3) {
+            targetAngle = -150;
+            oppositeAngle = 30;
+            // source
+        }
     }
 
     public void initialize() {
         alignmentPub.set(true);
 
-        // probably wrong numbers, also I'm not sure if it's correct in theory.
-        /*
-
-        if(element == 1){
-            targetVelocity = -90; 
-            // low goal 
-        }
-        else(element == 2){
-            targetVelocity = 180; 
-            // high goal 
-        }
-        else(element == 3){
-            targetVelocity = 150; 
-            // source 
-        } 
-        */
-       }
-    
-    /*We need to get the yaw angle
-     * Then set that closer to the current angle
-     * Continue until it's close enough
-     */
-    public void execute() {
-        if((drivetrain.getYaw()>(targetAngle+1)||drivetrain.getYaw()<(targetAngle-1))) {
-            angularVelocity = 0;
-            end(false);
+        if (targetAngle > 0) {
+            if (drivetrain.getYaw() < targetAngle && drivetrain.getYaw() < oppositeAngle) {
+                angularVelocityPub.set(0.8);
+            }
+            else {
+                angularVelocityPub.set(-0.8);
+            }
         }
         else {
-            //angular velocity is how much stuff should move. 
-            angularVelocity = (targetAngle-drivetrain.getYaw())/180;
+            if (drivetrain.getYaw() > targetAngle && drivetrain.getYaw() < oppositeAngle) {
+                angularVelocityPub.set(-0.8);
+            }
+            else {
+                angularVelocityPub.set(0.8);
+            }
         }
+       }
+    
+    /* We need to get the yaw angle
+     * Then set that closer to the current angle
+     * Continue until it's close enough
+     * Angular velocity is how fast the robot should rotate to the target angle
+    */
+    public void execute() {
     }
 
     public void end(boolean interrupted) {
         alignmentPub.set(false);
+        angularVelocityPub.set(0);
     }
 
     public boolean isFinished() {
-        return false;
+        return (drivetrain.getYaw()<(targetAngle+1) && drivetrain.getYaw()>(targetAngle-1));
     }
 }
