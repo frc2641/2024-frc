7b1505da19682ad47e705aa25d79fe6f334490ad
XxGladiatorxX1 <117952565+XxGladiatorxX1@users.noreply.github.com>
2024-06-02 23:39:30 -0400

    I will not be physically present to see whether this fix is a success, sadly, but I will make it nonetheless.

--- src/main/java/frc/team2641/robot2024/subsystems/Drivetrain.java
+++ src/main/java/frc/team2641/robot2024/subsystems/Drivetrain.java
@@ -228,20 +228,21 @@ public class Drivetrain extends SubsystemBase {
    *                         controls.
    * @param angularRotationX Angular velocity of the robot to set. Cubed for
    *                         smoother controls.
+   * @param robotRelative    Orient motion vectors of the robot relative to the
+   *                         front of the chassis.
    * @return Drive command.
    */
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX,
       BooleanSupplier robotRelative) {
-    CommandXboxController driverGamepad = new CommandXboxController(0);
     double magnitude = Math.sqrt(Math.pow(translationX.getAsDouble(), 2) + Math.pow(translationY.getAsDouble(), 2));
     double xComp = 
-      translationX.getAsDouble() < 0 ? -Math.pow(magnitude, 3) * Math.cos(Math.atan(driverGamepad.getLeftY()/driverGamepad.getLeftX())) :
-      Math.pow(magnitude, 3) * Math.cos(Math.atan(driverGamepad.getLeftY()/driverGamepad.getLeftX()));
+      translationX.getAsDouble() < 0 ? -Math.pow(magnitude, 3) * Math.cos(Math.atan(Math.abs(translationY.getAsDouble()/translationX.getAsDouble()))) :
+      translationX.getAsDouble() > 0 ? Math.pow(magnitude, 3) * Math.cos(Math.atan(Math.abs(translationY.getAsDouble()/translationX.getAsDouble()))) :
+      0.0;
     double yComp = 
-      translationY.getAsDouble() < 0 && magnitude * Math.sin(Math.atan(driverGamepad.getLeftY()/driverGamepad.getLeftX())) > 0 || 
-      translationY.getAsDouble() > 0 && magnitude * Math.sin(Math.atan(driverGamepad.getLeftY()/driverGamepad.getLeftX())) < 0 ? 
-      -Math.pow(magnitude, 3) * Math.sin(Math.atan(driverGamepad.getLeftY()/driverGamepad.getLeftX())) :
-      Math.pow(magnitude, 3) * Math.sin(Math.atan(driverGamepad.getLeftY()/driverGamepad.getLeftX()));
+      translationY.getAsDouble() < 0 ? -Math.pow(magnitude, 3) * Math.sin(Math.atan(Math.abs(translationY.getAsDouble()/translationX.getAsDouble()))) :
+      translationY.getAsDouble() > 0 ? Math.pow(magnitude, 3) * Math.sin(Math.atan(Math.abs(translationY.getAsDouble()/translationX.getAsDouble()))) :
+      0.0;
     return run(() -> {
       swerveDrive.drive(new Translation2d(xComp * swerveDrive.getMaximumVelocity(), yComp * swerveDrive.getMaximumVelocity()),
           Math.pow(angularRotationX.getAsDouble(), 3) * swerveDrive.getMaximumAngularVelocity(),
